8d3acf3c87aeb73ce9df17ad072b78d9762c3e44
Shiwangi1012 <shiwangisejal@gmail.com>
2025-05-03 01:44:34 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
-
-var t1 = gsap.timeline()
+function loadingAnimation(){
+    var t1 = gsap.timeline()
 t1.from(".line h1",{
     y:150,
     duration:0.6,
     delay:0.3,
     stagger:0.2
 })
-t1.from(".line1,.line h2",{
+t1.from(".line1, .line h2",{
     opacity: 0,
     onStart:function(){
         var h5text = document.querySelector(".line1 h5")
@@ -25,6 +24,18 @@ t1.from(".line1,.line h2",{
 })
 t1.to("#loader",{
     opacity:0,
-    duration:0.4,
+    duration:0.2,
     delay:4
 })
+gsap.from(".line",{
+    display:"none"
+})
+gsap.from(".page1",{
+    y:1200,
+    opacity:0,
+    duration:0.4,
+    delay:0.2
+})
+}
+
+loadingAnimation()
